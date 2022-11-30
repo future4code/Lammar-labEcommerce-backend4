8c3c486d7fa5bfcd605be47ac8fbd524cb922e19
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2022-11-30 20:31:12 -0300

    FEAT: Criação da lógica de registro de compras

---
+++ src/endpoints/registroDeCompras.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import { connection } from "../data/connection";
+
+export default async function registrarCompras(
+    req: Request,
+    res: Response
+){
+    try{
+        const {user_id, product_id , quantity} = req.body
+
+        if(!user_id || !product_id || !quantity){
+            return res.status(422).send("Insira todos os parâmetros necessários: user_id, product_id e quantity.")
+        }
+        
+        const precoProduto = await connection.raw( `
+            SELECT price from Labecommerce_products WHERE id=${product_id};
+        `)
+        .then(res => res[0][0].price)
+
+        const pedido = await connection.insert(
+            [{
+                id: Math.random(),
+                user_id: user_id,
+                product_id: product_id,
+                quantity: quantity,
+                total_price: quantity * precoProduto
+            }]
+        ).into("Labecommerce_purchases")
+        res.status(201).send("Pedido realizado com sucesso!")
+        console.log("Pedido realizado com sucesso!")
+
+    }catch(error:any){
+    }
+}
