519e034d7df20bc5f8a4cc04c52bd47a1b8545c3
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2022-12-06 17:19:16 -0300

    Correção na portaria

--- src/index.ts
+++ src/index.ts
@@ -35,6 +35,10 @@ app.post("/purchases", registrarCompras)
 app.get("/users/:user_id/purchases", listarCompras)
 
 const server = app.listen(process.env.PORT || 3003, () => {
-    console.log(`Servidor rodando na porta ${process.env.PORT || 3003}`)
-
+    if(server){
+        const address = server.address() as AddressInfo;
+        console.log(`Server is running in http://localhost:${address.port}`);
+    }else{
+        console.error(`Failure upon starting server.`)
+    }
 });
